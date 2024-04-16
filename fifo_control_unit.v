37763b11ce43bc2b5ff1b40a69a42c1d3d5a348c
adityacollective <163301086+adityacollective@users.noreply.github.com>
2024-04-16 21:38:53 +0530

    fifo_control_unit.v

--- fifo_control_unit.v
+++ fifo_control_unit.v
@@ -5,16 +5,3 @@ module fifo_control_unit(
   assign write_to_stk = write && (!stk_full);
   assign read_fr_stk = read && (!stk_empty);
 endmodule
-
-module fifo_datapath_unit #(parameter word_width = 32, stk_height = 8, stk_ptr_width = 3)(
-  output reg    [word_width-1 : 0]    data_out,
-  input         [word_width-1 : 0]    data_in,
-  input         [stk_ptr_width-1 : 0] write_ptr, read_ptr,
-  input                               write_to_stk, read_fr_stk,
-  input                               clk_write, clk_read, rst
-  );
-  reg           [word_width-1 : 0]    stk[stk_height-1 : 0];
-  
-  always@(posedge clk_write) if(write_to_stk) stk[write_ptr] <= data_in;
-  always@(posedge clk_read)  if(read_fr_stk) data_out <= stk[read_ptr];
-endmodule
